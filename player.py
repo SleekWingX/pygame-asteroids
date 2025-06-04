cc044ebb51cdf7b24aaa6f7b2620b732530f02ed
SleekWingX <alexisyazzie365@gmail.com>
2025-06-04 14:44:27 -0600

    limited bullet spew

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0.0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,14 +28,18 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
+        if self.timer > 0:
+            return None
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         shot = Shot(self.position.x + forward.x * self.radius, 
                     self.position.y + forward.y * self.radius, SHOT_RADIUS)
         shot.velocity = forward * PLAYER_SHOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
         return shot
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
